8420fb9a92fcc0912fae3da3bc2ccdb6098b6515
haithemalsaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-05 19:04:19 -0500

    TestDemo Commit

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub in Java world");
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+	}
+
+}
